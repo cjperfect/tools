6e5fc9560946fd56e816c1c13816a2c18042b015
cjperfect <1982145923@qq.com>
2023-01-30 17:20:22 +0800

    update

--- src/page/GenerateColumns/index.tsx
+++ src/page/GenerateColumns/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { Tabs, Button, message, Space } from "antd";
+import { Tabs, Button, message, Space, Empty } from "antd";
 import "./index.less";
 import FormInput from "./components/formInput";
 import CodeEditor from "components/CodeEditor";
@@ -84,49 +84,54 @@ const GenerateColumns: React.FC<IProps> = (props: IProps) => {
         <div className="code">
           {/* 右边预览区域 */}
           <h2 className="title">测试代码</h2>
-          <Tabs
-            defaultActiveKey="1"
-            items={[
-              {
-                key: "data",
-                label: "生成测试数据",
-                children: (
-                  <>
-                    <Button
-                      icon={<CopyOutlined />}
-                      onClick={() => {
-                        if (!result.dataText) return message.info("没有啥要复制的");
-                        copy(result.dataText);
-                        message.success("已复制到剪切板");
-                      }}
-                    >
-                      复制
-                    </Button>
-                    <CodeEditor value={result.dataText} language="javascript" />
-                  </>
-                )
-              },
-              {
-                key: "column",
-                label: "生成配置columns",
-                children: (
-                  <>
-                    <Button
-                      icon={<CopyOutlined />}
-                      onClick={() => {
-                        if (!result.columnsText) return message.info("没有啥要复制的");
-                        copy(result.columnsText);
-                        message.success("已复制到剪切板");
-                      }}
-                    >
-                      复制
-                    </Button>
-                    <CodeEditor value={result.columnsText} language="javascript" />
-                  </>
-                )
-              }
-            ]}
-          ></Tabs>
+          {result.dataText ? (
+            <Tabs
+              defaultActiveKey="1"
+              items={[
+                {
+                  key: "data",
+                  label: "生成测试数据",
+                  children: (
+                    <>
+                      <Button
+                        icon={<CopyOutlined />}
+                        onClick={() => {
+                          if (!result.dataText) return message.info("没有啥要复制的");
+                          copy(result.dataText);
+                          message.success("已复制到剪切板");
+                        }}
+                      >
+                        复制
+                      </Button>
+                      <CodeEditor value={result.dataText} language="javascript" />
+                    </>
+                  )
+                },
+                {
+                  key: "column",
+                  label: "生成配置columns",
+                  children: (
+                    <>
+                      <Button
+                        icon={<CopyOutlined />}
+                        onClick={() => {
+                          if (!result.columnsText) return message.info("没有啥要复制的");
+                          copy(result.columnsText);
+                          message.success("已复制到剪切板");
+                        }}
+                      >
+                        复制
+                      </Button>
+
+                      <CodeEditor value={result.columnsText} language="javascript" />
+                    </>
+                  )
+                }
+              ]}
+            ></Tabs>
+          ) : (
+            <Empty description="暂无数据" />
+          )}
         </div>
       </div>
 
